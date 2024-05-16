f212c17f34de820ba5318d580e46c5de2b943a26
Enesdrdnz <145133362+Enesdrdnz@users.noreply.github.com>
2024-05-16 22:31:58 +0300

    Create utils.py

---
+++ goingmodular/utils.py
@@ -0,0 +1,104 @@
+import torch
+import os
+from pathlib import Path
+from matplotlib import pyplot as plt
+from typing import Tuple,Dict,List
+"""
+File Containing various utility functions for Pytorch model training
+"""
+
+def save_model(model: torch.nn.Module,
+               target_dir: str,
+               model_name: str):
+  """Saves a PyTorch model to a target directory.
+
+  Args:
+    model: A target PyTorch model to save.
+    target_dir: A directory for saving the model to.
+    model_name: A filename for the saved model. Should include
+      either ".pth" or ".pt" as the file extension.
+  
+  Example usage:
+    save_model(model=model_0,
+               target_dir="models",
+               model_name="05_going_modular_tingvgg_model.pth")
+  """
+  # Create target directory
+  target_dir_path = Path(target_dir)
+  target_dir_path.mkdir(parents=True,
+                        exist_ok=True)
+  
+  # Create model save path
+  assert model_name.endswith(".pth") or model_name.endswith(".pt"), "model_name should end with '.pt' or '.pth'"
+  model_save_path = target_dir_path / model_name
+
+  # Save the model state_dict()
+  print(f"[INFO] Saving model to: {model_save_path}")
+  torch.save(obj=model.state_dict(),
+             f=model_save_path)
+def plot_loss_curves(results):
+    loss=results["train_loss"]
+    test_loss=results["test_loss"]
+    
+    accuracy=results["train_acc"]
+    test_accuracy=results["test_acc"] 
+    epochs=range(len(results["train_loss"]))
+    
+    plt.figure(figsize=(15,7))
+    
+    plt.subplot(1,2,1)
+    plt.plot(epochs,loss,label="train_loss")
+    plt.plot(epochs,test_loss,label="train_loss")
+    plt.title("Loss")
+    plt.xlabel("Epochs")
+    plt.legend()
+    
+    
+    plt.subplot(1,2,2)
+    plt.plot(epochs,accuracy,label="train_acc")
+    plt.plot(epochs,test_accuracy,label="test_acc")
+    plt.title("Accuracy")
+    plt.xlabel("Epochs") 
+    plt.legend()
+    
+
+    
+def display_random_images(dataset: torch.utils.data.dataset.Dataset,
+                          classes: List[str] = None,
+                          n: int = 10,
+                          display_shape: bool = True,
+                          seed: int = None):
+    
+    # 2. Adjust display if n too high
+    if n > 10:
+        n = 10
+        display_shape = False
+        print(f"For display purposes, n shouldn't be larger than 10, setting to 10 and removing shape display.")
+    
+    # 3. Set random seed
+    if seed:
+        random.seed(seed)
+
+    # 4. Get random sample indexes
+    random_samples_idx = random.sample(range(len(dataset)), k=n)
+
+    # 5. Setup plot
+    plt.figure(figsize=(16, 8))
+
+    # 6. Loop through samples and display random samples 
+    for i, targ_sample in enumerate(random_samples_idx):
+        targ_image, targ_label = dataset[targ_sample][0], dataset[targ_sample][1]
+
+        # 7. Adjust image tensor shape for plotting: [color_channels, height, width] -> [color_channels, height, width]
+        targ_image_adjust = targ_image.permute(1, 2, 0)
+
+        # Plot adjusted samples
+        plt.subplot(1, n, i+1)
+        plt.imshow(targ_image_adjust)
+        plt.axis("off")
+        if classes:
+            title = f"class: {classes[targ_label]}"
+            if display_shape:
+                title = title + f"\nshape: {targ_image_adjust.shape}"
+        plt.title(title)
+
